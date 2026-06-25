384b955f9e85b76998e55ee91cf708eaa1fc3ca2
maria navas <mafis_2707@hotmail.com>
2026-06-25 14:10:36 -0400

    We create our first python file!

---
+++ hello.py
@@ -0,0 +1,90 @@
+#print("Hello World")
+#print(2)
+
+#comment
+# this is a string example
+#learner ='Optimus prima'
+#print(learner)
+
+# this is a integer/number example
+#ideal_number_of_pets = 2
+#print(ideal_number_of_pets)
+
+# this is a float example
+#how_much_is_a_banana = 9.99
+#print(how_much_is_a_banana)
+
+# this is an example of a Boolean True/False
+#is_our_pet_vaccinated = True
+#print(is_our_pet_vaccinated)
+
+# datatype
+#print(type(learner))
+#print(type(ideal_number_of_pets))
+#print(type(how_much_is_a_banana))
+#print(type(is_our_pet_vaccinated))
+
+#is_it_a_number = 333
+#is_this_a_number = '333'
+
+#print(type(is_it_a_number))
+#print(type(is_this_a_number))
+
+#print(5+3)
+#print(5*3)
+#print(5-3)
+#print(5/3)
+#print(7/3)
+#print(10/3)
+#print(12/4)
+
+ # Example of a module
+#print(10%3)
+#print(10/3)
+#print(10%5)
+
+# Show an example of a exponent 'power to the 5th'
+#print(2**5)
+#print(32**73)
+
+# find the area of a rectangle
+#length = int(input('what is the legth? '))
+#width = int(input('what is the width? '))
+#area = length * width
+#print(f'The area of the rectangle is {area}')
+
+#Find the tax amount
+#price = 10
+#tax = price * 0.08
+#print(tax)
+
+#Find the average
+#avg = (10+10+10)/3
+#print(avg)
+
+#Ask the user 
+#fav_color = input('What is your favorite color? ')
+#print(f'your fav color is {fav_color}')
+
+#print(12, 24, -2, sep=':')
+#print('but', 'not', 'including', sep='**')
+#print('but', 'not', 'including', sep='')
+
+# Create a receipt
+print('Receipt')
+print('--------------------------')
+customerName = input("Enter customer's name: ")
+itemPrice = float(input('what is the cost of the item '))
+quantity = int(input('Enter quantity '))
+totalCost = itemPrice*quantity
+roundedCost = round(totalCost, 2)
+# customer Name
+print(f'Customer: {customerName}')
+# the price of what the purchased
+print(f'Item price: ${itemPrice}')
+# the quantity of the product they purchased
+print(f'Quantity: {quantity}')
+# the total cost
+print(f'Total cost ${roundedCost}')
+
+
